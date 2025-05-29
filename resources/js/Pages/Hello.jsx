18f45b001dd78c5cda54000a66ef76a8fb6aedfc
tamane-g <getianxuan@gmail.com>
2025-05-29 13:17:39 +0900

    [fix] コンポーネント名修正

--- resources/js/Pages/Hello.jsx
+++ resources/js/Pages/Hello.jsx
@@ -1,7 +1,7 @@
 import { Head } from '@inertiajs/react';
 import { Text } from '@mantine/core';
 
-export default function Dashboard() {
+export default function Hello() {
     return (
         <>
             <Head title="Hello World" />
