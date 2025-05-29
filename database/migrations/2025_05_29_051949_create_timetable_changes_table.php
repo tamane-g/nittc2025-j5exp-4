70053b77bccfc9cba255086a73075a24fe8f9a86
ss21074ss <ss21074@o-nisi.>
2025-05-29 14:58:18 +0900

    [WIP] 教室テーブルと授業変更テーブル、時間割テーブルを作成中

---
+++ database/migrations/2025_05_29_051949_create_timetable_changes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('timetable_changes', function (Blueprint $table) {
+            $table->id();
+            $table->date('original_time');
+            $table->date('updated_time');
+            $table->boolean('approval');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('timetable_changes');
+    }
+};
