936109d7d407147dead93776a48e863a07aa1a6f
Koba21057 <zyavariko1111@gmail.com>
2025-06-05 05:53:18 +0000

    　[WIP] 授業テーブル作成

---
+++ database/migrations/2025_06_05_055037_create_subject_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subject', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject', 20);
+            $table->integer('the_number_of_classes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subject');
+    }
+};
