9962e18f92f57713c975fb7a5c4b99beccd05d6b
ss21074ss <ss21074@o-nisi.>
2025-06-11 15:23:09 +0900

    時間割変更テーブルを作成しました

--- database/migrations/2025_05_29_053121_create_timetable_table.php
+++ database/migrations/2025_05_29_053121_create_timetable_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->enum('lesson', ['lesson_1', 'lesson_2', 'lesson_3', 'lesson_4']);
             $table->integer('grade');
             $table->integer('class');
-            $table->string('subject', 20);
             $table->string('teacher', 20);
             $table->integer('class_room_id');
             $table->integer('subject_id');
