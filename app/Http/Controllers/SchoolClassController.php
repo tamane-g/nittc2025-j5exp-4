05f765c6bde130720c98219c5aaff6828cbfe027
ss21074ss <ss21074@o-nisi.>
2025-07-17 23:16:47 +0900

    schoolclassコントローラ変更

--- app/Http/Controllers/SchoolClassController.php
+++ app/Http/Controllers/SchoolClassController.php
@@ -17,4 +17,30 @@ class SchoolClassController extends Controller
         return response()->json($reference);
     }
 
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'grade' => 'required|integer|min:1|max:6',
+            'class' => 'required|integer|min:1|max:10',
+        ]);
+
+        SchoolClass::create($validated);
+
+        return response()->json(['message' => '学級を登録しました']);
+    }
+
+    // 学級の削除
+    public function destroy($id)
+    {
+        $class = SchoolClass::find($id);
+
+         if (!$class) {
+            return response()->json(['message' => '学級が見つかりません'], 404);
+        }
+
+        $class->delete();
+
+        return response()->json(['message' => '学級を削除しました']);
+    }
+
 }
